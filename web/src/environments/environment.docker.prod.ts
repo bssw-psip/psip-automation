c22f4b9e9fc88892c2aa946a70013a8b8fa340e3
Greg Watson <g.watson@computer.org>
2021-08-13 10:45:27 -0400

    Initial commit of new UI

---
+++ web/src/environments/environment.docker.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://rateyourproject.org/ryp',
+    privacyUrl: 'https://www.cefriel.com/en/privacy',
+    enterprise: false
+};
